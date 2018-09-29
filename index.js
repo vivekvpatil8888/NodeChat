9a84c46d7386af39317a0f576910409dc759e232
Vivek Patil <vvp8212@rit.edu>
2018-09-29 01:07:59 -0400

    first and last commit

---
+++ index.js
@@ -0,0 +1,39 @@
+//following will extract the dependencies and include 
+var express = require('express');
+
+var socket = require('socket.io');
+
+var app = express();
+
+var server = app.listen(8000, function() {
+
+    console.log("The server is listening on the port 8000");
+});
+
+
+app.use(express.static('public'));
+
+//making socket connection
+
+var io = socket(server); //creates connection to server
+
+//when connection is done call following function.
+//
+io.on('connection', function(socket) {
+
+    socket.on("chat", function(data) {
+       
+        io.sockets.emit("chat", data);
+
+    });
+
+    // user typing event
+    socket.on("typing", function(data) {
+
+        socket.broadcast.emit("typing", data); // brodcast, it will be displayed on others windows
+
+    });
+
+});
+
+
